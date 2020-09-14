8c2e2c896848caace9eca4ddf0a8d4305300f437
Mel <jones.mel@gmail.com>
2020-09-14 17:53:09 +0200

    update fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      mono: ['IBM Plex Mono', 'monospace'],
+      body: ['Open Sans', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {},
