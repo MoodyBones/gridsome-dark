c3d7a0187cf70e2a2ce836e815c268018698594f
Mel <jones.mel@gmail.com>
2020-09-25 17:54:59 +0200

    new work together component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    class="max-w-screen-xl mx-auto pb-40 md:pr-24 
-    grid grid-cols-1 md:grid-cols-3 md:grid-flow-row gap-12"
+    class="max-w-screen-xl mx-auto pb-40
+    grid grid-cols-1 md:grid-cols-2 bg-secondary"
   >
     <div
       class="hero-heading-wrapper 
-      row-span-1 col-span-1 md:row-span-2 md:col-span-2
+      row-span-1 col-span-1 md:row-span-1 md:col-span-1
       sticky top-0 z-30
       opacity-50 rounded-sm w-full
       flex items-center justify-center"
     >
       <h1
-        class="text-6xl lg:text-9xl font-mono font-black text-center text-opacity-75 text-gray-900 p-24"
+        class="text-6xl lg:text-9xl font-display font-bold text-center text-opacity-75 text-gray-900 p-24"
         v-html="data.hero_title"
       ></h1>
     </div>
@@ -26,17 +26,16 @@
       <g-image src="~/assets/hand-eva-goncalves.png" class="w-56 h-auto z-20">
       </g-image>
     </div>
+    <WorkTogether class="md:row-start-4 md:col-span-1" />
     <div
       class="hero-subheading-wrapper z-50 
-      row-span-3 md:col-start-3 lg:col-span-1
+      row-span-3 md:col-start-2 lg:col-span-1
       rounded-sm bg-gray-100 text-gray-900 
       opacity-95
       px-4 py-10 md:px-10 
       flex justify-center"
     >
-      <h2
-        class="text-lg md:text-xl max-w-sm font-mono font-normal text-opacity-75"
-      >
+      <h2 class="text-lg md:text-6xl font-sans text-opacity-75">
         {{ data.hero_subtitle1 }}
         <br />
         <br />
@@ -54,9 +53,13 @@
 
 <script>
 import data from '@/data/theme.json'
+import WorkTogether from '@/components/WorkTogether.vue'
 
 export default {
   name: 'Hero',
+  components: {
+    WorkTogether,
+  },
   data() {
     return {
       data,
@@ -111,7 +114,7 @@ styles for text with bg image clip
     #f9f871
   ); */
   background-size: 400%;
-  animation: bg-animation 20s infinite alternate;
+  /* animation: bg-animation 20s infinite alternate; */
   h1 {
     width: min-content;
     /* text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1); */
@@ -119,7 +122,7 @@ styles for text with bg image clip
 }
 
 .hero-image-wrapper {
-  animation: shimmy 3s infinite alternate;
+  /* animation: shimmy 3s infinite alternate; */
 }
 
 .hero-subheading-wrapper {
---
+++ src/components/WorkTogether.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-3xl font-display">
+    <span class="block">{{ message }}</span>
+    <a class="text-primary font-bold" href="`${link}${contact}`"
+      ><span v-html="rightArrow"></span><span class="">{{ contact }}</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WorkTogether',
+  data() {
+    return {
+      message: 'Want to work together?',
+      contact: 'jones.mel@gmail.com',
+      link: 'mailto:',
+      rightArrow:
+        '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>',
+    }
+  },
+}
+</script>
