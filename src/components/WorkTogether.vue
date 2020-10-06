792edc2eda01f619a16ff9cb1923d7c014d34604
Mel <jones.mel@gmail.com>
2020-10-06 17:15:03 +0200

    remove bg animation

--- src/components/WorkTogether.vue
+++ src/components/WorkTogether.vue
@@ -5,10 +5,8 @@
   >
     <Smiley class="w-16 h-16" />
     <span class="block">{{ message }}</span>
-    <a class="opacity-50 uppercase font-bold" href="`${link}${contact}`">
-      <span class="">
-        {{ contact }}
-      </span>
+    <a class="text-primary uppercase font-bold" href="`${link}${contact}`">
+      {{ contact }}
     </a>
   </div>
 </template>
@@ -30,33 +28,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss" scoped>
-a {
-  span {
-    background: radial-gradient(
-      circle at top right,
-      #cd84f1,
-      #ffcccc,
-      #ff4d4d,
-      #ffaf40
-    );
-
-    background-clip: text;
-    -webkit-background-clip: text;
-    color: transparent;
-
-    background-size: 200%;
-    animation: bg-animation 20s infinite alternate;
-    text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
-  }
-}
-@keyframes bg-animation {
-  0% {
-    background-position: left;
-  }
-  100% {
-    background-position: right;
-  }
-}
-</style>
