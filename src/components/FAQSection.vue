130af1469a65256b721d9ff073afdc0624c066ac
Mel <jones.mel@gmail.com>
2020-10-08 09:29:06 +0200

    add icon to heading nd increase white space

--- src/components/FAQSection.vue
+++ src/components/FAQSection.vue
@@ -1,9 +1,12 @@
 <template>
   <section id="faq-section" class="grid justify-center bg-gray-200 pb-40">
     <h2
-      class="text-7xl md:text-8xl tracking-wide font-display font-bold text-center my-16 md:my-24"
+      class="text-7xl md:text-8xl tracking-wide font-display font-bold text-center md:text-left mt-40 mb-24"
     >
       {{ data.faq.title | capitalizeHeading }}
+      <span class="inline-block">
+        <ChatSVG class="w-16 h-16 md:w-20 md:h-20" />
+      </span>
     </h2>
     <FAQItem
       v-for="(item, index) in data.faq.questionList"
@@ -18,11 +21,13 @@
 <script>
 import data from '@/data/theme.json'
 import FAQItem from '@/components/FAQItem'
+import ChatSVG from '@/assets/chat.svg'
 
 export default {
   name: 'FAQSection',
   components: {
     FAQItem,
+    ChatSVG,
   },
   data() {
     return {
