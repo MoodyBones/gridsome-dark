cc62868cef58461a97756352afecf68dfa3b8843
Mel <jones.mel@gmail.com>
2020-10-19 17:12:08 +0200

    spacing tweaks

--- src/components/FAQItem.vue
+++ src/components/FAQItem.vue
@@ -5,29 +5,29 @@
     class=" grid grid-cols-1fr-11fr
          bg-gray-300
          cursor-pointer
-         mb-6 mx-2 md:mx-0
+         mb-12 mx-3 md:mx-0
          sm:max-w-screen-sm md:max-w-screen-md"
   >
     <span class="py-6 self-center">
       <Chevron
         :class="item.isOpen ? 'rotate-90' : 'rotate-0'"
         class="transition ease-out duration-200 
-        transform w-8 md:w-12 h-8 md:h-12 mx-3"
+        transform w-10 md:w-12 h-10 md:h-12 mx-4"
       />
     </span>
     <span
       class="font-display font-bold text-2xl md:text-3xl 
           self-center leading-snug
-        pr-6 py-4 "
+        pr-4 md:pr-6 py-5 md:py-6 "
     >
       {{ item.question }}
     </span>
     <div
       v-show="item.isOpen"
       class="bg-white col-span-2 
-          pt-6 pb-10 space-y-6
+          pt-6 pb-8 space-y-6
         grid grid-cols-1fr-11fr
-        text-lg md:text-xl tracking-wide leading-loose pr-6
+        text-lg md:text-xl tracking-wide leading-loose pr-6 md:pr-12
         sm:max-w-screen-sm md:max-w-screen-md"
     >
       <p
